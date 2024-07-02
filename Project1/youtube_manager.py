6162224146941224e615ed49ccd4b1c74243892d
mini-360 <rajabhay56999@gmail.com>
2024-07-02 12:12:17 +0530

    functionality to load and save video

--- Project1/youtube_manager.py
+++ Project1/youtube_manager.py
@@ -1,10 +1,29 @@
+import json
+
+
+def load_data():
+    try:
+        with open('log.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_data_helper(videos):
+    with open('log.txt', 'w') as file:
+        json.dump(videos, file)
+
 
 def list_all_videos(videos):
-    pass
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. ")
 
 
 def add_video(videos):
-    pass
+    name = input("Enter video name : ")
+    time = input("Enter video time : ")
+    videos.append({'name':name,'time':time})
+    save_data_helper(videos)
 
 
 def update_video(videos):
@@ -16,7 +35,7 @@ def delete_video(videos):
 
 
 def main():
-    videos = []
+    videos = load_data()
     while True:
         print("\n Youtube Manager | Choose an option ")
         print("1. List all YouTube videos ")
@@ -25,6 +44,7 @@ def main():
         print("4. Delete a YouTube video ")
         print("5. Exit the app ")
         choice = input("Enter your choice : ")
+        print(videos)
 
         match choice:
             case '1':
